a33c5f2b8a90deace5003613066ea2c6bb3e2deb
MarissaKnoll <50889439+MarissaKnoll@users.noreply.github.com>
2021-06-29 17:15:59 -0400

    Update and rename SharedSNVs.R to shared_snv_plot.R

--- R/shared_snv_plot.R
+++ R/shared_snv_plot.R
@@ -1,7 +1,7 @@
 # Plot to determine how many samples contain a variant
 # User inputs variant dataframe, and save directory
 
-SharedSNVs = function(vardf,savedir){
+shared_snv_plot = function(vardf){
   
   vardf$variant = paste0(vardf$CHROM,"_",vardf$major, vardf$POS, vardf$minor)
   
@@ -10,11 +10,10 @@ SharedSNVs = function(vardf,savedir){
   plot = ggplot(df, aes(x = POS, y = CHROM)) +
     geom_point(aes(size = totalsamp)) +
     ggtitle("Number of Samples Containing Each Variant") +
+    scale_y_discrete(limits=rev) +
     theme(legend.key = element_blank(),
           strip.background = element_rect(colour="black", fill="white"),
           axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
   
   print(plot)
-  ggsave(plot, filename = glue("{savedir}/SegmentSNVPlot.pdf"),
-                width = 12, height = 13.2, limitsize=FALSE)
 }
