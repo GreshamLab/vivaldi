bd6a5ab4aa036423dd9f8f9ec354f2e2c68abad5
kej310 <kej310@nyu.edu>
2021-07-08 14:45:45 -0400

    Update and rename TsTv_Plot.R to tstv_plot.R
    Removed ggsave, updated function name

--- R/tstv_plot.R
+++ R/tstv_plot.R
@@ -1,13 +1,7 @@
 # vivaldi package
 # Kate Johnson
 
-TsTv_Plot = function(df, wkdir, freq_cutoff = 0.02, coverage_cutoff = 300){
-
-    if (!dir.exists(glue("{wkdir}/SNV_figures"))) {
-        dir.create(glue("{wkdir}/SNV_figures"))
-      }
-
-    xlim = length(levels(factor(df$sample)))
+tstv_plot = function(df){
 
     p1 = ggplot(df, aes(x=sample,y=R)) +
         geom_point() +
@@ -20,12 +14,6 @@ TsTv_Plot = function(df, wkdir, freq_cutoff = 0.02, coverage_cutoff = 300){
 
     print(p1)
 
-    ggsave(p1,
-       filename = glue("{wkdir}SNV_figures/TsTv.K2P.{freq_cutoff}.{coverage_cutoff}.pdf"),
-       width = 0.3*xlim,
-       height = 6, limitsize=FALSE,
-          useDingbats = FALSE)
-
     p2 = ggplot(df, aes(x=sample,y=basic_tstv)) +
         geom_point() +
         theme_bw() +
@@ -36,12 +24,5 @@ TsTv_Plot = function(df, wkdir, freq_cutoff = 0.02, coverage_cutoff = 300){
             axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
     print(p2)
-
-    ggsave(p2,
-       filename = glue("{wkdir}SNV_figures/TsTv.basic.{freq_cutoff}.{coverage_cutoff}.pdf"),
-       width = 0.3*xlim,
-       height = 6, limitsize=FALSE,
-          useDingbats = FALSE)
-
 
 }
