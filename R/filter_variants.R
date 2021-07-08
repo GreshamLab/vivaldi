ab00f538346a2e986c6a91c49e330822b8a1958e
kej310 <kej310@nyu.edu>
2021-07-08 14:40:00 -0400

    Update and rename FiltVar.R to filter_variants.R
    Updated function name, added message of total SNP

--- R/filter_variants.R
+++ R/filter_variants.R
@@ -1,13 +1,16 @@
 # vivaldi package
 # Kate Johnson
 
-FiltVar = function(arranged_df, coverage_cutoff=300, frequency_cutoff=0.02){
+filter_variants = function(arranged_df, coverage_cutoff=300, frequency_cutoff=0.02){
 
+    input_dim = nrow(arranged_df)
     # using ALT_FREQ in case there isn't a 'minor variant' present, it still keeps consensus changes from ref
     arranged_df = arranged_df %>% filter(ALT_FREQ >= frequency_cutoff & gt_DP >= coverage_cutoff)
 
     arranged_df = arranged_df[!duplicated(arranged_df), ] %>% droplevels()
 
+    message("Total number of SNP filtered out: ", input_dim - nrow(arranged_df))
+    
     return(arranged_df)
 
 }
