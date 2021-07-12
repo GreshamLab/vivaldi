f1e96ce01857d3fad0a8eb4e6dfb83da367212de
kej310 <kej310@nyu.edu>
2021-07-12 16:08:04 -0400

    Added header

--- R/add_metadata.R
+++ R/add_metadata.R
@@ -1,7 +1,16 @@
-# vivaldi package
-# Kate Johnson
-
-# add meta information including size of segment/chromosomes
+#' add_metadata
+#'
+#' Adds metadata information to the vcf dataframe
+#'
+#' @name add_metadata
+#' @param df A rearranged vcf dataframe (arrange_gt_data)
+#' @param metadf A metadata dataframe
+#' @param by_vcf A vector of column names in the vcf dataframe that should be used to merge the vcf data with the metadata
+#' @param by_meta A vector of column names in the metadata dataframe that should be used to merge the metadata with the vcf data
+#' @return A vcf dataframe with metadata included
+#' @export
+#' @examples
+#' add_metadata(vcf_df, metadf, c("CHROM"), c("segment"))
 add_metadata = function(vcf_df, metadf, by_vcf, by_meta){
 
     return(merge(vcf_df, metadf, by.x=all_of(by_vcf), by.y=all_of(by_meta), all.x = TRUE))
