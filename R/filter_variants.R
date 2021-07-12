7c412eb87be6713d5ccd39590f37d568a51f8204
kej310 <kej310@nyu.edu>
2021-07-12 15:46:10 -0400

    Added header

--- R/filter_variants.R
+++ R/filter_variants.R
@@ -1,6 +1,15 @@
-# vivaldi package
-# Kate Johnson
-
+#' filter_variants
+#'
+#' Filters single-nucleotide variants using a coverage and frequency cutoff
+#'
+#' @name filter_variants
+#' @param arranged_df A rearranged vcf dataframe (rearranged using the arrange_gt_data function)
+#' @param coverage_cutoff The coverage cutoff for calling a SNV (default: 300x)
+#' @param frequency_cutoff Frequency cutoff for calling a SNV (default: 2%)
+#' @return A filtered vcf dataframe
+#' @export
+#' @examples
+#' filter_variants(arranged_df, 200, 0.02)
 filter_variants = function(arranged_df, coverage_cutoff=300, frequency_cutoff=0.02){
 
     input_dim = nrow(arranged_df)
