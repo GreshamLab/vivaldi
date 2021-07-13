e652e9eec1bbad87d364b18ad100a018647f24c5
kej310 <kej310@nyu.edu>
2021-07-13 16:33:50 -0400

    SNPeff variable list

---
+++ R/snpeff_info.R
@@ -0,0 +1,15 @@
+#' snpeff_info
+#'
+#' Returns vector containing information in snpeff annotations
+#'
+#' @name snpeff_info
+#' @return Returns vector containing information in snpeff annotations
+#' @export
+#' @examples
+#' snpeff_info()
+snpeff_info = function(){
+
+  return(c('allele','annotation','putative_impact','gene_name','gene_id',
+      'feature_type','feature_id','transcript_biotype','rank_total','HGVS.c','HGVS.p',
+      'cDNA_position','CDS_position','protein_position','distance_to_feature'))
+}
