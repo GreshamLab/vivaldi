72d098eab446094dccf78cdfbec3d04d13836283
kej310 <kej310@nyu.edu>
2021-07-13 16:29:04 -0400

    Added new plots
    Added chrom and genome plots. Normalized by genome/chrom kb.

--- R/tstv_plot.R
+++ R/tstv_plot.R
@@ -10,26 +10,55 @@
 #' tstv_plot(tstv_df)
 tstv_plot = function(df){
 
-    p1 = ggplot(df, aes(x=sample,y=R)) +
-        geom_point() +
-        theme_bw() +
-        ggtitle("K2P") +
-        ylab("Transition/Transversion ratio") +
-        theme(legend.key = element_blank(),
+    p1 = df %>% filter(chrom_or_genome == 'tstv_genome_count') %>% droplevels() %>%
+          ggplot(., aes(x=sample, y=tstv_ratio)) +
+            geom_point() +
+            theme_bw() +
+            ggtitle("Genome Ts/Tv") +
+            ylab("Genome Transition/Transversion ratio") +
+            theme(legend.key = element_blank(),
             strip.background = element_rect(colour="black", fill="white"),
             axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
     print(p1)
 
-    p2 = ggplot(df, aes(x=sample,y=basic_tstv)) +
-        geom_point() +
-        theme_bw() +
-        ggtitle("Basic Ts/Tv") +
-        ylab("Transition/Transversion ratio") +
-        theme(legend.key = element_blank(),
+    p2 = df %>% filter(chrom_or_genome == 'tstv_genome_count') %>% droplevels() %>%
+          ggplot(., aes(x=sample, y=tstv_ratio_perkb)) +
+            geom_point() +
+            theme_bw() +
+            ggtitle("Genome Ts/Tv per kb") +
+            ylab("Transition/Transversion ratio per genome kb") +
+            theme(legend.key = element_blank(),
             strip.background = element_rect(colour="black", fill="white"),
             axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
     print(p2)
+
+    p3 = df %>% filter(chrom_or_genome == 'tstv_chrom_count') %>% droplevels() %>%
+          ggplot(., aes(x=sample, y=tstv_ratio)) +
+            geom_point() +
+            theme_bw() +
+            ggtitle("Chrom Ts/Tv") +
+            ylab("Chrom Transition/Transversion ratio") +
+            theme(legend.key = element_blank(),
+            strip.background = element_rect(colour="black", fill="white"),
+            axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+          facet_grid(.~CHROM)
+
+    print(p3)
+
+    p4 = df %>% filter(chrom_or_genome == 'tstv_chrom_count') %>% droplevels() %>%
+          ggplot(., aes(x=sample, y=tstv_ratio_perkb)) +
+            geom_point() +
+            theme_bw() +
+            ggtitle("Chrom Ts/Tv per kb") +
+            ylab("Transition/Transversion ratio per chrom kb") +
+            theme(legend.key = element_blank(),
+            strip.background = element_rect(colour="black", fill="white"),
+            axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+            facet_grid(.~CHROM)
+
+
+    print(p4)
 
 }
