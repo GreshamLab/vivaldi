8b3bd6b27c3afb21a88056eb2b98f6f044474fc0
kej310 <kej310@nyu.edu>
2021-07-13 16:32:26 -0400

    Added annotation check
    Added an annotation check. Changed input variables

--- R/shannon_entropy.R
+++ R/shannon_entropy.R
@@ -4,23 +4,36 @@
 #'
 #' @name shannon_entropy
 #' @param df A rearranged vcf dataframe (arrange_gt_data)
-#' @param genome_size The full length of the genome
 #' @return A dataframe with Shannon entropy/kb calculations for the chroms and entire genome
 #' @export
 #' @examples
 #' shannon_entropy(df, 13000)
-shannon_entropy = function(df, genome_size){
+shannon_entropy = function(df){
   ## only uses positions with minorvariants
+  snpeff = snpeff_info()
+
+  if (length(intersect(colnames(df), snpeff)) > 0){
+
+    df = df %>% select(!all_of(c(snpeff)))
+
+    df = df[!duplicated(df), ] %>% droplevels()
+
+  } else{
+
+    df = df[!duplicated(df), ] %>% droplevels()
+
+  }
 
   df$shannon_ntpos = (-(df$majorfreq)*(log2(df$majorfreq))) + (-(df$minorfreq)*(log2(df$minorfreq)))
 
-  df = df %>% group_by(sample, CHROM) %>% mutate(chrom_shannon = sum(shannon_ntpos))
+  df = df %>% group_by(sample, CHROM, CHROM_SIZE) %>% mutate(chrom_shannon = sum(shannon_ntpos)) %>% ungroup()
 
-  df = df %>% group_by(sample) %>% mutate(genome_shannon = sum(chrom_shannon))
+  df = df %>% group_by(sample) %>% mutate(genome_shannon = sum(chrom_shannon)) %>% ungroup()
 
-  df$shannon_chrom_perkb = (df$chrom_shannon/(df$chrom_size/1000))
+  df$shannon_chrom_perkb = (df$chrom_shannon/(df$CHROM_SIZE/1000))
 
-  df$genome_shannon_perkb = (df$genome_shannon/(genome_size/1000))
+  df$genome_shannon_perkb = (df$genome_shannon/(df$GENOME_SIZE/1000))
 
   return(df)
+
 }
