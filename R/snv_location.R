b92a390bf00a646b0249ae631c01120307625e1e
kej310 <kej310@nyu.edu>
2021-07-13 16:24:46 -0400

    Added annotation check. Renamed variables

--- R/snv_location.R
+++ R/snv_location.R
@@ -3,28 +3,38 @@
 #' Reads in the vcf dataframe and generates a plot showing the frequency and location of SNVs
 #'
 #' @name snv_location
-#' @param vcf_df A rearranged (arrange_gt_data)
+#' @param df A rearranged (arrange_gt_data)
 #' @return A plot showing the location and frequency of SNVs found across samples
 #' @export
 #' @examples
-#' snv_location(vcf_df)
-snv_location = function(vcf_df){
+#' snv_location(df)
+snv_location = function(df){
 
-    xlim = length(levels(factor(vcf_df$CHROM)))
+    snpeff = snpeff_info()
 
-    ylim = length(levels(factor(vcf_df$sample)))
+    if (length(intersect(colnames(df), snpeff)) > 0){
+
+        df = df %>% select(!all_of(c(snpeff)))
+
+        df = df[!duplicated(df), ] %>% droplevels()
+
+      } else{
+
+        df = df[!duplicated(df), ] %>% droplevels()
+
+    }
 
     plot4 = ggplot() +
-        geom_point(data = vcf_df %>% filter(ALT_TYPE == 'minor') %>% droplevels(),
+        geom_point(data = df %>% filter(ALT_TYPE == 'minor') %>% droplevels(),
                       aes(x=POS, y=minorfreq, color=minor)) +
 
-        geom_point(data = vcf_df %>% filter(ALT_TYPE == 'major') %>% droplevels(),
+        geom_point(data = df %>% filter(ALT_TYPE == 'major') %>% droplevels(),
                       aes(x=POS, y=majorfreq, color=major)) +
 
-        geom_rug(data = vcf_df %>% filter(ALT_TYPE == 'minor') %>% droplevels(),
+        geom_rug(data = df %>% filter(ALT_TYPE == 'minor') %>% droplevels(),
                       aes(x=POS, y=minorfreq, color=minor)) +
 
-        geom_rug(data = vcf_df %>% filter(ALT_TYPE == 'minor') %>% droplevels(),
+        geom_rug(data = df %>% filter(ALT_TYPE == 'minor') %>% droplevels(),
                       aes(x=POS, y=minorfreq, color=minor)) +
 
         theme_bw() +
@@ -34,6 +44,8 @@ snv_location = function(vcf_df){
                         axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
         facet_grid(sample~CHROM,  scales = 'free_x')
 
-    print(plot4)
+    return(ggplotly(plot4))
+
+    #print(plot4)
 
 }
