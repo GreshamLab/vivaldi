bbcfcf878cac001c3fd6a849c2bcd126955d5bd3
MarissaKnoll <50889439+MarissaKnoll@users.noreply.github.com>
2021-07-15 13:39:25 -0400

    Update shared_snv_table.R
    duplication check

--- R/shared_snv_table.R
+++ R/shared_snv_table.R
@@ -14,6 +14,8 @@ shared_snv_table = function(vardf){
   vardf$variant = paste0(vardf$CHROM,"_",vardf$major, vardf$POS, vardf$minor)
   df = group_by(vardf, variant) %>% mutate(count = 1, totalsamp = sum(count)) 
   
+  df = df[!duplicated(df), ] %>% droplevels()
+  
   ordered_df = select(df, CHROM,POS,REF,ALT,allele,annotation,feature_type,feature_id,protein_position,
                       HGVS.p,majorfreq,minorfreq,variant,totalsamp) %>%
     arrange(desc(totalsamp))
