2dcb37c10214af6987231fbea12abcecc3a8bf8e
Mohammed Khalfan <mk5636@nyu.edu>
2022-09-20 22:30:08 -0400

    use package names explicitly with double colon operator

--- R/shared_snv_plot.R
+++ R/shared_snv_plot.R
@@ -3,27 +3,27 @@
 #' Reads in a dataframe that has been arranged (arrange_gt_data) and filtered (filter_variants) and outputs plots
 #'
 #' @name shared_snv_plot
-#' @param vardf A rearranged (arrange_gt_data) and filtered (filtered_variants) vcf dataframe 
+#' @param vardf A rearranged (arrange_gt_data) and filtered (filtered_variants) vcf dataframe
 #' @return A plot showing the location of variants and the number of samples that conatain each variant
 #' @export
 #' @examples
 #' shared_snv_plot(vardf)
 
 shared_snv_plot = function(vardf){
-  
+
   vardf$variant = paste0(vardf$CHROM,"_",vardf$major, vardf$POS, vardf$minor)
-  
-  df = group_by(vardf, variant) %>% mutate(count = 1, totalsamp = sum(count)) 
-  
+
+  df = group_by(vardf, variant) %>% mutate(count = 1, totalsamp = sum(count))
+
   df = df[!duplicated(df), ] %>% droplevels()
-  
-  plot = ggplot(df, aes(x = POS, y = CHROM)) +
-    geom_point(aes(size = totalsamp)) +
-    ggtitle("Number of Samples Containing Each Variant") +
-    scale_y_discrete(limits=rev) +
-    theme(legend.key = element_blank(),
-          strip.background = element_rect(colour="black", fill="white"),
-          axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-  
+
+  plot = ggplot2::ggplot(df, ggplot2::aes(x = POS, y = CHROM)) +
+    ggplot2::geom_point(ggplot2::aes(size = totalsamp)) +
+    ggplot2::ggtitle("Number of Samples Containing Each Variant") +
+    ggplot2::scale_y_discrete(limits=rev) +
+    ggplot2::theme(legend.key = ggplot2::element_blank(),
+          strip.background = ggplot2::element_rect(colour="black", fill="white"),
+          axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1))
+
   print(plot)
 }
