34245278551054cab09311c983303e201d797730
Mohammed Khalfan <mk5636@nyu.edu>
2022-09-20 22:28:18 -0400

    use package names explicitly with double colon operator

--- R/snv_genome.R
+++ R/snv_genome.R
@@ -3,25 +3,25 @@
 #' Reads in a dataframe that has been arranged (arrange_gt_data) and filtered (filter_variants) and outputs plots
 #'
 #' @name snv_genome
-#' @param vardf A rearranged (arrange_gt_data) and filtered (filtered_variants) vcf dataframe 
+#' @param vardf A rearranged (arrange_gt_data) and filtered (filtered_variants) vcf dataframe
 #' @return A bar plot showing the number of variants per sample colored by their SNPEff annotation
 #' @export
 #' @examples
 #' snv_genome(vardf)
 
 snv_genome = function(vardf){
-  
+
   sum_df = group_by(vardf, sample,annotation) %>% tally()
-  
+
   sum_df = sum_df[!duplicated(sum_df), ] %>% droplevels()
-  
-  plot = ggplot(sum_df, aes(x = sample , y = n, fill = annotation)) +
-    geom_col() +
-    ggtitle("Number of Variants per Genome") +
-    theme(legend.key = element_blank(),
-          strip.background = element_rect(colour="black", fill="white"),
-          axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-  
+
+  plot = ggplot2::ggplot(sum_df, ggplot2::aes(x = sample , y = n, fill = annotation)) +
+    ggplot2::geom_col() +
+    ggplot2::ggtitle("Number of Variants per Genome") +
+    ggplot2::theme(legend.key = ggplot2::element_blank(),
+          strip.background = ggplot2::element_rect(colour="black", fill="white"),
+          axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1))
+
   print(plot)
 
 }
