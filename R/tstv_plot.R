50e76d03b4fd9cd182224bd151705e82ff8e97e9
Mohammed Khalfan <mk5636@nyu.edu>
2022-09-20 22:30:44 -0400

    use package names explicitly with double colon operator

--- R/tstv_plot.R
+++ R/tstv_plot.R
@@ -11,52 +11,52 @@
 tstv_plot = function(df){
 
     p1 = df %>% filter(chrom_or_genome == 'tstv_genome_count') %>% droplevels() %>%
-          ggplot(., aes(x=sample, y=tstv_ratio)) +
-            geom_point() +
-            theme_bw() +
-            ggtitle("Genome Ts/Tv") +
-            ylab("Genome Transition/Transversion ratio") +
-            theme(legend.key = element_blank(),
-            strip.background = element_rect(colour="black", fill="white"),
-            axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+          ggplot2::ggplot(., ggplot2::aes(x=sample, y=tstv_ratio)) +
+      ggplot2::geom_point() +
+      ggplot2::theme_bw() +
+      ggplot2::ggtitle("Genome Ts/Tv") +
+      ggplot2::ylab("Genome Transition/Transversion ratio") +
+      ggplot2::theme(legend.key = ggplot2::element_blank(),
+            strip.background = ggplot2::element_rect(colour="black", fill="white"),
+            axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1))
 
     print(p1)
 
     p2 = df %>% filter(chrom_or_genome == 'tstv_genome_count') %>% droplevels() %>%
-          ggplot(., aes(x=sample, y=tstv_ratio_perkb)) +
-            geom_point() +
-            theme_bw() +
-            ggtitle("Genome Ts/Tv per kb") +
-            ylab("Transition/Transversion ratio per genome kb") +
-            theme(legend.key = element_blank(),
-            strip.background = element_rect(colour="black", fill="white"),
-            axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+      ggplot2::ggplot(., ggplot2::aes(x=sample, y=tstv_ratio_perkb)) +
+      ggplot2::geom_point() +
+      ggplot2::theme_bw() +
+      ggplot2::ggtitle("Genome Ts/Tv per kb") +
+      ggplot2::ylab("Transition/Transversion ratio per genome kb") +
+      ggplot2::theme(legend.key = ggplot2::element_blank(),
+            strip.background = ggplot2::element_rect(colour="black", fill="white"),
+            axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1))
 
     print(p2)
 
     p3 = df %>% filter(chrom_or_genome == 'tstv_chrom_count') %>% droplevels() %>%
-          ggplot(., aes(x=sample, y=tstv_ratio)) +
-            geom_point() +
-            theme_bw() +
-            ggtitle("Chrom Ts/Tv") +
-            ylab("Chrom Transition/Transversion ratio") +
-            theme(legend.key = element_blank(),
-            strip.background = element_rect(colour="black", fill="white"),
-            axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
-          facet_grid(.~CHROM)
+      ggplot2::ggplot(., ggplot2::aes(x=sample, y=tstv_ratio)) +
+      ggplot2::geom_point() +
+      ggplot2::theme_bw() +
+      ggplot2::ggtitle("Chrom Ts/Tv") +
+      ggplot2::ylab("Chrom Transition/Transversion ratio") +
+      ggplot2::theme(legend.key = ggplot2::element_blank(),
+            strip.background = ggplot2::element_rect(colour="black", fill="white"),
+            axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1)) +
+      ggplot2::facet_grid(.~CHROM)
 
     print(p3)
 
     p4 = df %>% filter(chrom_or_genome == 'tstv_chrom_count') %>% droplevels() %>%
-          ggplot(., aes(x=sample, y=tstv_ratio_perkb)) +
-            geom_point() +
-            theme_bw() +
-            ggtitle("Chrom Ts/Tv per kb") +
-            ylab("Transition/Transversion ratio per chrom kb") +
-            theme(legend.key = element_blank(),
-            strip.background = element_rect(colour="black", fill="white"),
-            axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
-            facet_grid(.~CHROM)
+      ggplot2::ggplot(., ggplot2::aes(x=sample, y=tstv_ratio_perkb)) +
+      ggplot2::geom_point() +
+      ggplot2::theme_bw() +
+      ggplot2::ggtitle("Chrom Ts/Tv per kb") +
+      ggplot2::ylab("Transition/Transversion ratio per chrom kb") +
+      ggplot2::theme(legend.key = ggplot2::element_blank(),
+            strip.background = ggplot2::element_rect(colour="black", fill="white"),
+            axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1)) +
+      ggplot2::facet_grid(.~CHROM)
 
 
     print(p4)
