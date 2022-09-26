d0edae68195a8d0bb387129d5e521f2e4cedaad3
MarissaKnoll <mrk444@nyu.edu>
2022-09-26 18:32:55 -0400

    include avgfreq and new rep metadata

--- R/merge_replicates.R
+++ R/merge_replicates.R
@@ -3,23 +3,29 @@
 #' Merges replicate VCF files into a single dataframe
 #'
 #' @name merge_replicates
-#' @param vardir Directory path containing vcf files
-#' @param column Directory path containing vcf files
-#' @param nameofrep1 Directory path containing vcf files
-#' @param nameofrep2 Directory path containing vcf files
-#' @param commoncols Directory path containing vcf files
-#' @return
+#' @param vardir Data frame of variants
+#' @param vardir Data frame of replicate information
+#' @param nameofrep1 Name of variable representing the first replicate, must be written with quotes
+#' @param nameofrep2 Name of variable representing the second replicate
+#' @param commoncols List of columns to merge the replicates by
+#' @return a data frame containing replicate information
 #' @export
 #' @examples
 #' merge_replicates(vardir, column, nameofrep1, nameofrep2, commoncols)
-merge_replicates = function(vardir, column, nameofrep1, nameofrep2, commoncols){
+merge_replicates = function(vardir, repdata, nameofrep1, nameofrep2,commoncols){
 
-  vardir_rep1 = filter(vardir, column == nameofrep1)
+  df = merge(vardir, repdata, by = c("sample"))
+
+  vardir_rep1 = filter(df, replicate == nameofrep1)
   # nameofrep1 must be given in ""
-  vardir_rep2 = filter(vardir, column == nameofrep2)
+  vardir_rep2 = filter(df, replicate == nameofrep2)
   # nameofrep2 must be given in ""
 
   vardir_merged = merge(vardir_rep1, vardir_rep2, by = commoncols)
   vardir_merged = vardir_merged[!duplicated(vardir_merged), ]
+
+  vardir_merged$avg_freq = (vardir_merged$minorfreq.x + vardir_merged$minorfreq.y) / 2
+
+  return(vardir_merged)
 
 }
