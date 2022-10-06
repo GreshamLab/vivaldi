c526a44b7c63f2d5e1f301bfb097930c0bdc481e
Mohammed Khalfan <mk5636@nyu.edu>
2022-10-06 14:59:54 -0400

    tally function is from dplyr package

--- R/tally_it.R
+++ R/tally_it.R
@@ -28,7 +28,7 @@ tally_it = function(df, groupit, new_colname){
 
     }
 
-    count_df = df %>% group_by_at(vars(all_of(groupit))) %>% tally()
+    count_df = df %>% group_by_at(vars(all_of(groupit))) %>% dplyr::tally()
 
     colnames(count_df)[colnames(count_df) == 'n'] = new_colname
 
