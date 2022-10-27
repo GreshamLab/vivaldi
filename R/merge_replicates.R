ab11dfb137540301f754c0adf2d6f251886763e6
MarissaKnoll <mrk444@nyu.edu>
2022-10-27 17:52:41 -0400

    Update merge_replicates.R

--- R/merge_replicates.R
+++ R/merge_replicates.R
@@ -26,19 +26,13 @@ merge_replicates = function(vardf, repdata, nameofrep1, nameofrep2,commoncols){
   df_merged = merge(df_rep1, df_rep2, by = commoncols)
   df_merged = df_merged[!duplicated(df_merged), ]
 
-  if (nrow(df_merged) > 0){
+  df_merged$minorfreq = (df_merged$minorfreq.x + df_merged$minorfreq.y) / 2
+  df_merged$majorfreq = (df_merged$majorfreq.x + df_merged$majorfreq.y) / 2
 
-    df_merged$minorfreq = (df_merged$minorfreq.x + df_merged$minorfreq.y) / 2
-    df_merged$majorfreq = (df_merged$majorfreq.x + df_merged$majorfreq.y) / 2
+  # add weighted average using read counts
 
-    # add weighted average using read counts
-
-    df_merged$weighted_minorfreq = (df_merged$minorcount.x + df_merged$minorcount.y) / (df_merged$gt_DP.x + df_merged$gt_DP.y)
-    df_merged$weighted_majorfreq = (df_merged$majorcount.x + df_merged$majorcount.y) / (df_merged$gt_DP.x + df_merged$gt_DP.y)
-
-
-  } else{message("Merging columns do not match: ")}
-
+  df_merged$weighted_minorfreq = (df_merged$minorcount.x + df_merged$minorcount.y) / (df_merged$gt_DP.x + df_merged$gt_DP.y)
+  df_merged$weighted_majorfreq = (df_merged$majorcount.x + df_merged$majorcount.y) / (df_merged$gt_DP.x + df_merged$gt_DP.y)
 
   return(df_merged)
 
