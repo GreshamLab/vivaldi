c190a009b43f8766c901654887a2fb8dff8ef83f
MarissaKnoll <mrk444@nyu.edu>
2022-10-27 18:38:24 -0400

    Updating filtering

--- R/filter_variants.R
+++ R/filter_variants.R
@@ -10,13 +10,25 @@
 #' @export
 #' @examples
 #' \dontrun{
-#' filter_variants(df, coverage_cutoff = 200, frequency_cutoff = 0.02)
+#' filter_variants(df, coverage_cutoff = 200, frequency_cutoff = 0.03)
 #' }
-filter_variants = function(df, coverage_cutoff=300, frequency_cutoff=0.02){
+filter_variants = function(df, coverage_cutoff=200, frequency_cutoff=0.03){
 
     input_dim = nrow(df)
     # using ALT_FREQ in case there isn't a 'minor variant' present, it still keeps consensus changes from ref
-    df = df %>% dplyr::filter(ALT_FREQ >= frequency_cutoff & gt_DP >= coverage_cutoff)
+    # for loop assures this function works on data with and without replicates
+    if ("ALT_FREQ" %in% names(df) = TRUE){
+
+      df = df %>% dplyr::filter(ALT_FREQ >= frequency_cutoff &
+                                  gt_DP >= coverage_cutoff)
+
+    }else{
+
+      df = df %>% dplyr::filter(ALT_FREQ.x >= frequency_cutoff & ALT_FREQ.y >= frequency_cutoff &
+                                  gt_DP.x >= coverage_cutoff & gt_DP.y >= coverage_cutoff)
+
+    }
+
 
     df = df[!duplicated(df), ] %>% droplevels()
 
