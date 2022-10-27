2ce43c425c1aaec157166622ee05576c9d1b5c80
MarissaKnoll <mrk444@nyu.edu>
2022-10-27 17:17:29 -0400

    Update arrange_gt_data.R

--- R/arrange_gt_data.R
+++ R/arrange_gt_data.R
@@ -96,7 +96,7 @@ arrange_gt_data = function(vardir, reference_fasta, annotated = 'yes', ntlist=c(
                                          majorcount = ifelse(ALT_TYPE == "major", ALT_COUNT,REF_COUNT),
                                          minorcount = ifelse(ALT_TYPE == "minor", ALT_COUNT,REF_COUNT))
 
-    all_files = select(all_files, c(sample,CHROM,POS,REF,ALT,ANN,
+    all_files = dplyr::select(all_files, c(sample,CHROM,POS,REF,ALT,ANN,
                                     gt_DP,REF_COUNT,ALT_COUNT,REF_FREQ,ALT_FREQ,ALT_TYPE,
                                     major,minor,majorfreq,minorfreq,majorcount,minorcount))
 
