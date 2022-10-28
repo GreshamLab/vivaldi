cfec103a85eb9698e9390f67a45f5d3f1dc2ee79
MarissaKnoll <mrk444@nyu.edu>
2022-10-28 14:14:33 -0400

    including option to only compare certain samples into shared_snv_plot

--- R/shared_snv_plot.R
+++ R/shared_snv_plot.R
@@ -4,14 +4,36 @@
 #'
 #' @name shared_snv_plot
 #' @param vardf A rearranged (arrange_gt_data) and filtered (filtered_variants) vcf dataframe
+#' @param samples A vector of samples to be compared (default = "no")
 #' @return A plot showing the location of variants and the number of samples that contain each variant
 #' @export
 #' @examples
 #' \dontrun{
-#' shared_snv_plot(vardf)
+#' shared_snv_plot(vardf, samples = "no")
 #' }
 
 shared_snv_plot = function(vardf){
+
+  if(samples = "yes"){
+
+    dplyr::filter(vardf, sample %in% samplelist)
+
+    vardf$variant = paste0(vardf$CHROM,"_",vardf$major, vardf$POS, vardf$minor)
+
+    df = dplyr::group_by(vardf, variant) %>% dplyr::mutate(count = 1, totalsamp = sum(count))
+
+    df = df[!duplicated(df), ] %>% droplevels()
+
+    plot = ggplot2::ggplot(df, ggplot2::aes(x = POS, y = CHROM)) +
+      ggplot2::geom_point(ggplot2::aes(color = totalsamp)) +
+      ggplot2::ggtitle("Number of Samples Containing Each Variant") +
+      ggplot2::scale_y_discrete(limits=rev) +
+      ggplot2::theme(legend.key = ggplot2::element_blank(),
+                     strip.background = ggplot2::element_rect(colour="black", fill="white"),
+                     axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1))
+
+    print(plot)
+  }else{
 
   vardf$variant = paste0(vardf$CHROM,"_",vardf$major, vardf$POS, vardf$minor)
 
@@ -28,4 +50,6 @@ shared_snv_plot = function(vardf){
           axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1))
 
   print(plot)
+  }
 }
+
