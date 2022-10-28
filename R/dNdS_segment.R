65ec5cb1b59dd86c59af61ab7333c437b449fab5
MarissaKnoll <mrk444@nyu.edu>
2022-10-28 13:29:03 -0400

    updating dNdS function with dplyr:: syntax

--- R/dNdS_segment.R
+++ R/dNdS_segment.R
@@ -15,21 +15,21 @@ dNdS_segment = function(annotation_df){
 
   annotation_df$feature_id = factor(annotation_df$feature_id, levels = SPLICEFORMS)
 
-  df_dN = filter(annotation_df, annotation == "missense_variant" | annotation == "nonsense_variant") %>%
-    group_by(sample, feature_id) %>% droplevels() %>% tally()
-  df_dS = filter(annotation_df, annotation == "synonymous_variant") %>%
-    group_by(sample, feature_id) %>% droplevels() %>% tally()
+  df_dN = dplyr::filter(annotation_df, annotation == "missense_variant" | annotation == "nonsense_variant") %>%
+    dplyr::group_by(sample, feature_id) %>% droplevels() %>% dplyr::tally()
+  df_dS = dplyr::filter(annotation_df, annotation == "synonymous_variant") %>%
+    dplyr::group_by(sample, feature_id) %>% droplevels() %>% dplyr::tally()
 
-  df = merge(df_dN, df_dS,  by = c("sample","feature_id")) %>% select(sample,feature_id,n.x,n.y)
+  df = dplyr::merge(df_dN, df_dS,  by = c("sample","feature_id")) %>% dplyr::select(sample,feature_id,n.x,n.y)
   df$dNdS = (df$n.x) / (df$n.y)
 
-  plot = ggplot(df, aes(x = sample , y = dNdS)) +
-    geom_point() +
-    facet_grid(~feature_id) +
-    ggtitle("dNdS Ratio Per Splice Form") +
-    theme(legend.key = element_blank(),
+  plot = dplyr::ggplot(df,dplyr:: aes(x = sample , y = dNdS)) +
+    dplyr::geom_point() +
+    dplyr::facet_grid(~feature_id) +
+    dplyr::ggtitle("dNdS Ratio Per Splice Form") +
+    dplyr::theme(legend.key = element_blank(),
           strip.background = element_rect(colour="black", fill="white"),
           axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
-  print(plot)
+  dplyr::print(plot)
 }
