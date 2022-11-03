be958033032e61b12af2a3c941744c1c1ac6627e
genericdata <eb167@nyu.edu>
2022-11-03 10:53:10 -0400

    global variables

---
+++ R/globals.R
@@ -0,0 +1,40 @@
+globalVariables(unique(c(
+  # af_distribution:
+  "minorfreq",
+  # arrange_data:
+  "ALT", "ALT_COUNT", "ALT_FREQ", "ALT_TYPE", "ANN", "CHROM", "gt_AD", "major", "majorcount", "majorfreq", "minor", "minorcount", "minorfreq", "POS", "REF", "REF_COUNT", "REF_FREQ",
+  # arrange_gt_data:
+  "ALT", "ALT_COUNT", "ALT_FREQ", "ALT_TYPE", "ANN", "CHROM", "gt_AD", "gt_DP", "major", "majorcount", "majorfreq", "minor", "minorcount", "minorfreq", "POS", "REF", "REF_COUNT", "REF_FREQ",
+  # arrange_gt_data_varscan:
+  "ALT", "ALT_FREQ", "ALT_TYPE", "gt_AD", "gt_RD", "REF", "REF_FREQ",
+  # arrange_no_gt_data:
+  "ALT", "ALT_FREQ", "ALT_TYPE", "REF", "REF_FREQ",
+  # dNdS_segment:
+  "annotation", "dNdS", "feature_id", "n.x", "n.y", "SPLICEFORMS",
+  # filter_variants:
+  "ALT_FREQ", "ALT_FREQ.x", "ALT_FREQ.y", "gt_DP", "gt_DP.x", "gt_DP.y",
+  # plot_shannon:
+  "CHROM", "genome_shannon_perkb", "POS", "shannon_chrom_perkb", "shannon_ntpos",
+  # position_allele_freq:
+  "CHROM", "major", "majorfreq", "minor", "minorfreq", "POS",
+  # prepare_annotations:
+  "ANN",
+  # read_reference_fasta_dna:
+  "CHROM", "CHROM_SIZE", "V1",
+  # shannon_entropy:
+  "CHROM", "chrom_shannon", "SegmentSize", "shannon_ntpos",
+  # shared_snv_plot:
+  "CHROM", "count", "DF_filt", "POS", "totalsamp", "variant",
+  # shared_snv_table:
+  "ALT", "annotation", "CHROM", "count", "feature_id", "feature_type", "HGVS.p", "major", "majorfreq", "minor", "minorfreq", "POS", "protein_position", "REF", "totalsamp", "variant",
+  # snv_genome:
+  "annotation", "n",
+  # snv_location:
+  "ALT_TYPE", "major", "majorfreq", "minor", "minorfreq", "POS",
+  # snv_segment:
+  "annotation", "CHROM", "n",
+  # tstv_plot:
+  ".", "chrom_or_genome", "tstv_ratio_perkb",
+  # tstv_ratio:
+  "tstv", "tstv_chrom_count"
+)))
