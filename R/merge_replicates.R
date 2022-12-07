c237af8b3903004724a7c9f7f0f83679a2408ece
genericdata <eb167@nyu.edu>
2022-12-07 15:56:59 -0500

    example

--- R/merge_replicates.R
+++ R/merge_replicates.R
@@ -11,10 +11,25 @@
 #' @return a data frame containing replicate information
 #' @export
 #' @examples
-#' cols = c("sample","CHROM","POS","REF","ALT","ANN","ALT_TYPE","major","minor")
+#'
+#' # The `merge_replicates()` functions generates a dataframe with all variants
+#' # that are found in both sequencing replicates. It excludes variants that are
+#' # only found in one replicate. In addition, average frequencies are computed
+#' # from the two replicates.
+#'
+#' # `VCF_DF` dataframe shows a pair of replicates and their variants at 3 positions.
+#' VCF_DF[ , c("sample", "CHROM", "POS")]
+#'
+#' # Merge `VCF_DF` and `replicates` dataframes by the following columns
+#' cols = c("sample","CHROM","POS","REF","ALT","ALT_TYPE","major","minor")
+#'
 #' DF_reps = merge_replicates(VCF_DF,replicates,"rep1","rep2",cols)
-#' dim(DF_reps)
-#' DF_reps
+#'
+#' # The dataframe now contains the 2 variants at positions 234 & 240 that were
+#' # detected in both sequencing replicates whereas variant at  position 254
+#' # was only in a single replicate so it was removed during the merge.
+#'
+#' DF_reps[, c("sample","CHROM","POS","minorfreq","majorfreq","weighted_minorfreq","weighted_majorfreq")]
 #'
 merge_replicates = function(vardf, repdata, nameofrep1, nameofrep2,commoncols){
 
